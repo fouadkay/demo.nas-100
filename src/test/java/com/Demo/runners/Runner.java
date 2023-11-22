cc9ce3cca6550f57b5b8b8a2cdfd18edc04bb563
fouad <karamifouad2@hotmail.com>
2023-11-22 13:38:43 -0500

    changed the t back

--- src/test/java/com/Demo/runners/Runner.java
+++ src/test/java/com/Demo/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/Demo/step_definitions",
         dryRun = false,
-        tags =" @runthisbitch",
+        tags =" @runthishoe",
         publish = true
 )
 public class Runner {} 
